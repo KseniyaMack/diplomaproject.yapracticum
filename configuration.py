feae834e2ff586f68a4be2e328038c998b7642c5
Ксения Головина <kseniyamal2@gmail.com>
2024-10-18 00:41:07 +1200

    Commit

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://24967e39-e5e6-4180-9fbc-66fb88d64110.serverhub.praktikum-services.ru/"
+CREATE_ORDER_PATH = "/api/v1/orders"
+GET_ORDER_BY_TRACK = "/api/v1/orders/track"
---
+++ data.py
@@ -0,0 +1,13 @@
+order_body = {
+    "firstName": "Sponge",
+    "lastName": "Bob",
+    "address": "Konoha, 142 apt.",
+    "metroStation": 4,
+    "phone": "+7 800 355 35 35",
+    "rentTime": 5,
+    "deliveryDate": "2024-10-17",
+    "comment": "Saske, come back to Konoha",
+    "color": [
+        "BLACK"
+    ]
+}
